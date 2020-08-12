9d961280062f2c557f2bd12a3901132a5abf4eb7
Restyled.io <commits@restyled.io>
2020-08-12 19:45:27 +0000

    Restyled by clang-format

--- api/avatar.js
+++ api/avatar.js
@@ -3,8 +3,7 @@ const crypto = require("crypto");
 const contrast = require("contrast");
 
 const hexToDec = (hex) => {
-  let result = 0,
-    digitValue;
+  let result = 0, digitValue;
   hex = hex.toLowerCase();
   for (var i = 0; i < hex.length; i++) {
     digitValue = "0123456789abcdefgh".indexOf(hex[i]);
@@ -13,29 +12,22 @@ const hexToDec = (hex) => {
   return result;
 };
 
-const avrg = (x, y) => {
-  return (hexToDec(x) + hexToDec(y)) / 2;
-};
+const avrg = (x, y) => { return (hexToDec(x) + hexToDec(y)) / 2; };
 
 module.exports = (req, res) => {
   let name = req.query.uname;
 
   if (name)
     name = (name.split(" ")[0][0] + name.split(" ")[1][0]).toUpperCase();
-  else name = "HI";
+  else
+    name = "HI";
 
-  const hash = crypto
-    .createHash("md5")
-    .update(req.query.usr || "test")
-    .digest("hex");
-  const colors = [hash.substr(0, 6), hash.substr(6, 6)];
+  const hash =
+      crypto.createHash("md5").update(req.query.usr || "test").digest("hex");
+  const colors = [ hash.substr(0, 6), hash.substr(6, 6) ];
 
   console.log(avrg(colors[0], colors[1]).toString(16));
-  console.log(
-      contrast(
-          avrg(colors[0], colors[1]).toString(16)
-          )
-      );
+  console.log(contrast(avrg(colors[0], colors[1]).toString(16)));
 
   res.send(svg(colors[0], colors[1], name));
 };
