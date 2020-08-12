5d05d2fb8b67a7b4ca638c9ed21f998b1ecfd782
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-12 21:15:23 +0200

    Initials

--- api/avatar.js
+++ api/avatar.js
@@ -2,7 +2,13 @@ const svg = require('./svg');
 const crypto = require('crypto');
 
 module.exports = (req, res) => {
+
+    let name = req.query.uname;
+
+    if(name) name = name.split(' ')[0][0]+name.split(' ')[1][0];
+    else name = "HI";
+
     const hash = crypto.createHash('md5').update(req.query.usr || 'test').digest('hex');
     const colors = [hash.substr(0, 6), hash.substr(6, 6)];
-    res.send(svg(colors[0], colors[1], "FT"));
+    res.send(svg(colors[0], colors[1], name));
 };
