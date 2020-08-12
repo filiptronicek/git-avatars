dcc1edbfcf2379dd038e6a322cb0057cbe72c960
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-12 17:41:22 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -11,7 +11,7 @@ function getAvatarURL($username, $provider)
 }
 $url = getAvatarURL($_GET['u'], $_GET['p']);
 if (strpos($url, 's=80') == true) {
-    $url = str_replace('s=80', 's=512', $url);
+    $url = str_replace('s=80', 's=460', $url);
 }
 
 $data = file_get_contents($url);
