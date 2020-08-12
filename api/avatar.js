38a6b2baeeb78b6b09060578d02939b87919a91e
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-12 20:55:55 +0200

    Name param

--- api/avatar.js
+++ api/avatar.js
@@ -4,5 +4,5 @@ const crypto = require('crypto');
 module.exports = (req, res) => {
     const hash = crypto.createHash('md5').update(req.query.usr || 'test').digest('hex');
     const colors = [hash.substr(0, 6), hash.substr(6, 6)];
-    res.send(svg(colors[0], colors[1]));
+    res.send(svg(colors[0], colors[1], "FT"));
 };
--- api/svg.js
+++ api/svg.js
@@ -1,4 +1,4 @@
-function gen(start, stop) {
+function gen(start, stop, name) {
     return (
     `<svg width="120" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <defs>
@@ -9,7 +9,7 @@ function gen(start, stop) {
         </defs>
         
         <rect x="10" y="120" rx="15" ry="15" width="100" height="100" fill="url(#Gradient2)"/>
-        <text x="50%" y="35%" alignment-baseline="central" dominant-baseline="central" text-anchor="middle" fill="#fff" font-family="sans-serif" font-size="35px">Bruh</text>
+        <text x="50%" y="35%" alignment-baseline="central" dominant-baseline="central" text-anchor="middle" fill="#fff" font-family="sans-serif" font-size="35px">${name}</text>
     </svg>
     `
     );
