a254ee2ce64cffe69ef8ee0a0de2c5625218e3da
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-12 19:56:14 +0200

    Default value

--- api/avatar.js
+++ api/avatar.js
@@ -2,7 +2,7 @@ const svg = require('./svg');
 const crypto = require('crypto');
 
 module.exports = (req, res) => {
-    const hash = crypto.createHash('md5').update(req.query.usr).digest('hex');
+    const hash = crypto.createHash('md5').update(req.query.usr || 'test').digest('hex');
     const colors = [hash.substr(0, 6), hash.substr(6, 6)];
     res.send(svg(colors[0], colors[1]));
 };
