cbfcf0c59a7d0c609134837fdfc327d7875881a3
Restyled.io <commits@restyled.io>
2020-08-13 15:20:46 +0000

    Restyled by clang-format

--- api/avatar.js
+++ api/avatar.js
@@ -1,4 +1,4 @@
-import { createHash } from "crypto";
+import {createHash} from "crypto";
 // import contrast from "contrast";
 
 import svg from "./svg";
@@ -9,14 +9,12 @@ export default (req, res) => {
 
   if (name) {
     name = (name.split(" ")[0][0] + name.split(" ")[1][0]).toUpperCase();
-  }
-  else name = "HI";
+  } else
+    name = "HI";
 
-  const hash = createHash("md5")
-    .update(req.query.usr || "test")
-    .digest("hex");
+  const hash = createHash("md5").update(req.query.usr || "test").digest("hex");
 
-  const colors = [hash.substr(0, 6), hash.substr(6, 6)];
+  const colors = [ hash.substr(0, 6), hash.substr(6, 6) ];
   // const average = mix(colors[0], colors[1]);
 
   const pColor = "#ffffff";
