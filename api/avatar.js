aae6b13b086c18ac296a000133569f413606d58b
Restyled.io <commits@restyled.io>
2020-08-13 13:58:38 +0000

    Restyled by clang-format

--- api/avatar.js
+++ api/avatar.js
@@ -9,18 +9,18 @@ module.exports = (req, res) => {
 
   if (name)
     name = (name.split(" ")[0][0] + name.split(" ")[1][0]).toUpperCase();
-  else name = "HI";
+  else
+    name = "HI";
 
-  const hash = crypto
-    .createHash("md5")
-    .update(req.query.usr || "test")
-    .digest("hex");
+  const hash =
+      crypto.createHash("md5").update(req.query.usr || "test").digest("hex");
 
-  const colors = [hash.substr(0, 6), hash.substr(6, 6)];
+  const colors = [ hash.substr(0, 6), hash.substr(6, 6) ];
   const average = mix(colors[0], colors[1]);
 
   const pColor = "#ffffff";
-  if (contrast(average) === "light") pcolor = "#000000";
+  if (contrast(average) === "light")
+    pcolor = "#000000";
 
   res.send(svg(colors[0], colors[1], name, pColor));
 };
