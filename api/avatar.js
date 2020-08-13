4578da5002fd92c55f8a5e51bc3c398249459f00
Restyled.io <commits@restyled.io>
2020-08-13 20:04:34 +0000

    Restyled by prettier

--- api/avatar.js
+++ api/avatar.js
@@ -1,4 +1,4 @@
-import {createHash} from "crypto";
+import { createHash } from "crypto";
 // import contrast from "contrast";
 
 import svg from "./svg";
@@ -9,16 +9,17 @@ export default (req, res) => {
 
   if (name) {
     name = (name.split(" ")[0][0] + name.split(" ")[1][0]).toUpperCase();
-  } else
-    name = "HI";
+  } else name = "HI";
 
-  const hash = createHash("md5").update(req.query.usr || "test").digest("hex");
+  const hash = createHash("md5")
+    .update(req.query.usr || "test")
+    .digest("hex");
 
-  const colors = [ hash.substr(0, 6), hash.substr(6, 6) ];
+  const colors = [hash.substr(0, 6), hash.substr(6, 6)];
   // const average = mix(colors[0], colors[1]);
 
   const pColor = "#ffffff";
   // if (contrast(average) === "light") pcolor = "#000000";
-  res.setHeader('Content-Type', 'image/svg+xml');
+  res.setHeader("Content-Type", "image/svg+xml");
   res.send(svg(colors[0], colors[1], name, pColor));
 };
--- api/svg.js
+++ api/svg.js
@@ -1,5 +1,5 @@
 const gen = (start, stop, name, color) =>
-    `<svg width="120" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  `<svg width="120" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <defs>
             <linearGradient id="Gradient2" x1="0" x2="0" y1="0" y2="1">
                 <stop offset="0%" stop-color="#${start}"/>
@@ -8,8 +8,7 @@ const gen = (start, stop, name, color) =>
         </defs>
 
         <rect x="0" y="0" rx="15" ry="15" width="120" height="120" fill="url(#Gradient2)"/>
-        <text x="50%" y="50%" alignment-baseline="central" dominant-baseline="central" text-anchor="middle" fill="${
-        color}" font-family="sans-serif" font-size="45px">${name}</text>
+        <text x="50%" y="50%" alignment-baseline="central" dominant-baseline="central" text-anchor="middle" fill="${color}" font-family="sans-serif" font-size="45px">${name}</text>
     </svg>
     `;
 
